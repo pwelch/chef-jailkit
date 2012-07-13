1d438eff16fe0ca63af113b21a8c9d6058509a64
Paul Welch <welch.paulallen@gmail.com>
2012-07-13 16:39:03 -0400

    update repo

--- tmp/cookbooks/cookbook/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Paul Welch"
-maintainer_email "pwelch2004@gmail.com"
-license          "Apache 2.0"
-description      "Installs Jailkit"
-version          "0.0.1"
-
-%w{ubuntu debian}.each do |os|
-  supports os
-end
